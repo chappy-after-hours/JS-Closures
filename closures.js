e5d258ce88943e871f53f1e049ee702e882903e0
Jeff Chapman <jeffcchapman@gmail.com>
2017-01-16 14:45:45 -0700

    finished 3

--- closures.js
+++ closures.js
@@ -68,13 +68,20 @@ makeCall("435-215-9248");
 properly. */
 
 //Code Here
+function makeCounter() {
+  var counter = 0;
+  return function() {
+    counter++;
+    return counter;
+  }
+}
 
-//Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+// Uncomment this once you make your function
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
